ac59f8716b1508941995a472f92b8a39011cef25
kenneth133 <kenneth.k.lee@gmail.com>
2016-07-21 18:55:05 -0400

    add parsers

--- tables/scores_staging.tbl.sql
+++ tables/scores_staging.tbl.sql
@@ -10,10 +10,10 @@ create table dbo.scores_staging (
   strategy_cd	varchar(16)		NULL,
 
   mqa_id		varchar(32)		NULL,
-  ticker		varchar(32)		NULL,
+  ticker		varchar(16)		NULL,
   cusip			varchar(32)		NULL,
   sedol			varchar(32)		NULL,
-  isin			varchar(32)		NULL,
+  isin			varchar(64)		NULL,
   currency_cd	varchar(3)		NULL,
   exchange_nm	varchar(60)		NULL,
 
